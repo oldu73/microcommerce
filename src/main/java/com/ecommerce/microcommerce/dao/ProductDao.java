aec36af4be3b0aea1b673630f80727962433b6e8
Olivier Durand <olivier.durand@logifleet.ch>
2020-01-14 14:43:17 +0100

    1. Utilisez JPA pour communiquer avec une base de données - notation JPA (JPQL)

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -17,8 +17,7 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
 
     List<Product> findByNomLike(String recherche);
 
-    //todo debug (nice to have), do not work, maybe JPQL syntax is'nt correct?!
-//    @Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
-//    List<Product> chercherUnProduitCher(@Param("prixLimit") int prix);
+    @Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
+    List<Product> chercherUnProduitCher(@Param("prixLimit") int prix);
 
 }
--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -1,12 +1,14 @@
 package com.ecommerce.microcommerce.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Product {
 
     @Id
+    @GeneratedValue
     private int id;
 
     private String nom;
