1704c70dcbb741c3cd2e07ceea73edadb3d0a7de
oldu73 <oldu73@gmail.com>
2020-01-18 11:25:03 +0100

    Documentation swagger

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.ecommerce.microcommerce.web.controller;
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 import com.ecommerce.microcommerce.web.exceptions.ProductNotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@Api(description = "API pour les opérations CRUD sur les produits.")
 @RestController
 public class ProductController {
 
@@ -24,34 +27,10 @@ public class ProductController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     //Récupérer la liste des produits
-    @RequestMapping(value = "/Produits", method = RequestMethod.GET)
+//    @RequestMapping(value = "/Produits", method = RequestMethod.GET)
+    @GetMapping(value = "/Produits")
     public List<Product> listeProduits() {
         return productDao.findAll();
-    }
-
-    //Récupérer un produit par son Id
-    @GetMapping(value = "/Produits/{id}")
-    public Product afficherUnProduit(@PathVariable int id) {
-
-        Product product = productDao.findById(id);
-
-        if (product == null) throw new ProductNotFoundException("Product with id " + id + " not found!");
-
-        return product;
-    }
-
-    @GetMapping(value = "test/prix/{prixLimit}")
-    public List<Product> testeDeRequetes(@PathVariable int prixLimit) {
-
-        //        return productDao.findByPrixGreaterThan(prixLimit);
-
-        //Java Persistence Query Language (JPQL)
-        return productDao.chercherUnProduitCher(prixLimit);
-    }
-
-    @GetMapping(value = "test/nom/{recherche}")
-    public List<Product> testeDeRequetes(@PathVariable String recherche) {
-        return productDao.findByNomLike("%"+recherche+"%");
     }
 
     //Ajouter un produit
@@ -75,14 +54,39 @@ public class ProductController {
         return ResponseEntity.created(location).build();
     }
 
-    @DeleteMapping (value = "/Produits/{id}")
-    public void supprimerProduit(@PathVariable int id) {
-        productDao.deleteById(id);
-    }
-
     @PutMapping (value = "/Produits")
     public void updateProduit(@RequestBody Product product) {
         productDao.save(product);
     }
 
+    //Récupérer un produit par son Id
+    @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
+    @GetMapping(value = "/Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+
+        Product product = productDao.findById(id);
+
+        if (product == null) throw new ProductNotFoundException("Product with id " + id + " not found!");
+
+        return product;
+    }
+
+    @DeleteMapping (value = "/Produits/{id}")
+    public void supprimerProduit(@PathVariable int id) {
+        productDao.deleteById(id);
+    }
+
+    @GetMapping(value = "test/prix/{prixLimit}")
+    public List<Product> testeDeRequetes(@PathVariable int prixLimit) {
+
+        //        return productDao.findByPrixGreaterThan(prixLimit);
+
+        //Java Persistence Query Language (JPQL)
+        return productDao.chercherUnProduitCher(prixLimit);
+    }
+
+    @GetMapping(value = "test/nom/{recherche}")
+    public List<Product> testeDeRequetes(@PathVariable String recherche) {
+        return productDao.findByNomLike("%"+recherche+"%");
+    }
 }
